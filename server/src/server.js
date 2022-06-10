e36dfed2d1f165cc3d8a56e8ce7c062ae21b54a0
Arshdeep Sandhu <arshsarhali@gmail.com>
2022-06-09 23:14:51 -0700

    Added backend for Planets List

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const app =require('./app');
+
+const server = http.createServer(app)
+
+const {loadPlanetsData} = require('./models/planets.model')
+
+const PORT= process.env.PORT || 8000;
+async function startServer(){
+await loadPlanetsData();
+
+server.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}.....`)
+})
+}
+
+startServer();
