5e165c817dc428b01645d14e3f58b590021744ef
Arshdeep Sandhu <arshsarhali@gmail.com>
2022-07-06 22:46:26 -0700

    Added API Version functionality

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 
-const planetsRouter = require('./routes/planets/planets.router')
-const launchesRouter = require('./routes/launches/launches.router')
+const api = require('./routes/api');
+
+
 
 
 app.use(cors({
@@ -14,8 +15,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'..','public')))
 
-app.use('/planets',planetsRouter)
-app.use('/launches',launchesRouter)
+app.use('/v1',api)
 
 app.get('*',(req,res)=>{
 res.sendFile(path.join(__dirname,'..','public','index.html'))
